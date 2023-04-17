a0ce7c6d3f2f00d300d5011bdab219de00b31ecf
Naresh9919 <110754714+naresh9919@users.noreply.github.com>
2023-04-17 15:25:40 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -2,27 +2,34 @@
 
 sudo apt update -y
 
-sudo apt install default-jre -y
+$ sudo apt install openjdk-11-jre
 
 java -version
 
 sudo apt update -y
 
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
 
-sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-
-sudo apt update -y
+sudo apt-get update -y
 
 sudo add-apt-repository universe -y
 
-sudo apt-get install jenkins -y
+sudo apt-get install jenkins
 
-sudo service jenkins start
+sudo systemctl enable jenkins
+
+sudo systemctl start jenkins
+
+sudo systemctl status jenkins
+
+
+
 
 cat /var/lib/jenkins/secrets/initialAdminPassword
-
-#sudo service jenkins status
 
 
 #34.232.26.121
